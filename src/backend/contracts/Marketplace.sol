57e4b6cd2e97b398795e9b851127022edce297b2
Abhinav Sharma <abhinavsharma2001jk77@gmail.com>
2023-10-16 16:16:43 +0530

    Added the makeItem function

--- src/backend/contracts/Marketplace.sol
+++ src/backend/contracts/Marketplace.sol
@@ -8,13 +8,55 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "hardhat/console.sol";
 
 contract Marketplace is ReentrancyGuard {
-    // Variables
     address payable public immutable feeAccount; // the account that receives fees
     uint public immutable feePercent; // the fee percentage on sales
     uint public itemCount;
 
+    struct Item {
+        uint itemId;
+        IERC721 nft;
+        uint tokenId;
+        uint price;
+        address payable seller;
+        bool sold;
+    }
+
+    event Offered(
+        uint itemId,
+        address indexed nft,
+        uint tokenId,
+        uint price,
+        address indexed seller
+    );
+
+    mapping(uint => Item) public items;
+
     constructor(uint _feePercent) {
         feeAccount = payable(msg.sender);
         feePercent = _feePercent;
+    }
+
+    // Make item to offer on the marketplace
+    function makeItem(
+        IERC721 _nft,
+        uint _tokenId,
+        uint _price
+    ) external nonReentrant {
+        require(_price > 0, "Price must be greater than zero");
+        // increment itemCount
+        itemCount++;
+        // transfer nft
+        _nft.transferFrom(msg.sender, address(this), _tokenId);
+        // add new item to items mapping
+        items[itemCount] = Item(
+            itemCount,
+            _nft,
+            _tokenId,
+            _price,
+            payable(msg.sender),
+            false
+        );
+        // emit Offered event
+        emit Offered(itemCount, address(_nft), _tokenId, _price, msg.sender);
     }
 }
